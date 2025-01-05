4044159f59c3847eb49a7e797cbf07dae0212519
Tharindu Pramuditha <tharindutpk@gmail.com>
2025-01-05 22:54:26 +0530

    Add tiny changes to wezterm config

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -1,13 +1,17 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 15
+config.line_height = 1.2
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
