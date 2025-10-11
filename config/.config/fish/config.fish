703d25918b88f0abe6c458902d8ed1c26906d7a8
Tharindu Pramuditha <tharindutpk@gmail.com>
2025-10-11 12:46:20 +0530

    Change font sizes and zellij theme

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Aliases
+alias cfg="cd ~/.config"
+alias dls="cd ~/.dotfiles"
+alias cn="cd ~/.config/nvim"
+alias obs="cd ~/code/personal/obsidian"
