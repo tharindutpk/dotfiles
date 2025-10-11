7230100960c923abe568a1d4e8f40a8f7035fd1a
Tharindu Pramuditha <tharindutpk@gmail.com>
2025-10-11 14:57:37 +0530

    Add paths to fish

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -7,3 +7,11 @@ alias cfg="cd ~/.config"
 alias dls="cd ~/.dotfiles"
 alias cn="cd ~/.config/nvim"
 alias obs="cd ~/code/personal/obsidian"
+
+# Node (N)
+set -Ux N_PREFIX $HOME/.n
+
+# Fish paths
+fish_add_path -m $N_PREFIX/bin
+fish_add_path -m /opt/homebrew/bin
+fish_add_path -m /opt/homebrew/sbin
