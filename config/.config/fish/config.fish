7f9941ed0f2a823aa1b850848945ed659dc81f70
Tharindu Pramuditha <git@tharindu.co>
2026-09-14 20:48:13 +0530

    fish: clean up config

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,20 +1,26 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set fish_greeting
+set -g fish_greeting
 set -g fish_key_bindings fish_vi_key_bindings
+set -gx EDITOR nvim
+set -gx N_PREFIX $HOME/.n
 
-# Aliases
-alias cfg="cd ~/.config"
-alias dls="cd ~/.dotfiles"
-alias cn="cd ~/.config/nvim"
-alias obs="cd ~/Notes/obsidian/"
+# One call, first entry wins. Folders that don't exist yet are skipped.
+fish_add_path -g ~/.local/bin $N_PREFIX/bin ~/.cargo/bin \
+  /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/curl/bin \
+  /Applications/Ghostty.app/Contents/MacOS
 
-# Node (N)
-set -Ux N_PREFIX $HOME/.n
+if status is-interactive
+  abbr -a cfg 'cd ~/.config'
+  abbr -a dls 'cd ~/.dotfiles'
+  abbr -a cn 'cd ~/.config/nvim'
+  abbr -a obs 'cd ~/Notes/obsidian'
 
-# Fish paths
-fish_add_path -m $N_PREFIX/bin
-fish_add_path -m /opt/homebrew/bin
-fish_add_path -m /opt/homebrew/sbin
+  abbr -a ts tmux-sessionizer
+  abbr -a ta 'tmux attach -t'
+  abbr -a tl 'tmux ls'
+  abbr -a tn 'tmux new -s'
+  abbr -a tk 'tmux kill-session -t'
+
+  # Ctrl-F: fuzzy-switch tmux projects, same as prefix + f inside tmux.
+  bind --mode insert ctrl-f 'tmux-sessionizer; commandline -f repaint'
+  bind --mode default ctrl-f 'tmux-sessionizer; commandline -f repaint'
+end
--- config/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-function fish_user_key_bindings
-    # Ctrl-F anywhere: fuzzy-switch tmux projects. Same script the tmux
-    # prefix + f binding runs, so the muscle memory is identical inside and
-    # outside a session.
-    bind --mode insert ctrl-f 'tmux-sessionizer; commandline -f repaint'
-    bind --mode default ctrl-f 'tmux-sessionizer; commandline -f repaint'
-end
